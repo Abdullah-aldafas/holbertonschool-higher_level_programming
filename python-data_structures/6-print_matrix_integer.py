5f625d2ba8acae39b32c382c665c53c9b57f2b90
Abdullah <assf23134@icloud.com>
2025-07-23 08:48:53 +0000

    Solved Task:6. Lists of lists = Matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
         for i in matrix:
-            for j in range(len(i)):
-                if matrix == [[]]:
+            if matrix == [[]]:
                     print()
                     return
+            for j in range(len(i)):
                 if j != len(i) - 1:
                     print("{:d}".format(i[j]), end=" ")
                 else:
