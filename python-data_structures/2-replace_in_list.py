00a8d015e128119d5437600cff9d1e92ce4ab078
Abdullah <assf23134@icloud.com>
2025-07-23 07:47:55 +0000

    Solved Task:2. Replace element

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,7 +2,8 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    
+
     list_copy = my_list
     list_copy[idx] = element
     return list_copy
+    
