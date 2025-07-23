cdb423cbe356182a397081bd08e3338c98ed110e
Abdullah <assf23134@icloud.com>
2025-07-23 08:21:10 +0000

    Solved Task:5. Can you C me now?

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -4,4 +4,5 @@ def no_c(my_string):
     for i in my_string:
         if i != "c" and i != "C":
             new = new + i
-    return new  # return "".join([i for i in my_string if i != "c" and i != "C"])
+    return new
+# return "".join([i for i in my_string if i != "c" and i != "C"])
