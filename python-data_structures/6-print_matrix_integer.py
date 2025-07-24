016f42db1de363cc5e4f0af64c8367cbbd5e31ae
Abdullah <Assf23134@icloud.com>
2025-07-24 10:25:21 +0000

    Solved Task: a function that prints a matrix of integers.

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-        for i in matrix:
-            if matrix == [[]]:
-                    print()
-                    return
+    for i in matrix:
+        if matrix == [[]]:
+            print()
+            return
             for j in range(len(i)):
                 if j != len(i) - 1:
                     print("{:d}".format(i[j]), end=" ")
