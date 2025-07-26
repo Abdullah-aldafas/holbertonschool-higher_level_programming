1ea8595a87321cb31f41a335129d4c62507b2307
Abdullah <assf23134@icloud.com>
2025-07-26 15:28:50 +0000

    Solved task:4. Text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -11,13 +11,14 @@ def text_indentation(text):
     """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    i = 0
-    while i < len(text):
-        print(text[i], end="")
-        if text[i] in ".?:":
-            print("\n")
-            i += 1
-            while i < len(text) and text[i] == " ":
-                i += 1
-            continue
-        i += 1
+
+    buffer = ""
+    for char in text:
+        buffer += char
+        if char in ".?:":
+            print(buffer.strip())
+            print()
+            buffer = ""
+
+        if buffer.strip():
+            print(buffer.strip())
