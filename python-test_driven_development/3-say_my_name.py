df4202605687b62b6f1f65248f58f1ff541b07f5
Abdullah <assf23134@icloud.com>
2025-07-26 15:03:36 +0000

    Solved Task:2. Say my name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,6 +5,9 @@ This module defines a function that prints a full name.
 
 
 def say_my_name(first_name, last_name=""):
+    """
+    Prints a full name in the format 'My name is <first_name> <last_name>'
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
