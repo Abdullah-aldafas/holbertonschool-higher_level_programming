db92e5accf54b47a6925837a463b27f882d63d81
Abdullah <assf23134@icloud.com>
2025-07-26 14:27:30 +0000

    Solved Task:0. Integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 """
 This module defines a function that adds two integers
 """
@@ -8,9 +6,10 @@ This module defines a function that adds two integers
 def add_integer(a, b=98):
     """
     Adds two integers or floats and returns the result as an integer.
-    """  
-    if type(a) is not int and type(a) is not float:  #  if not isinstance(a, (int, float)):
+    """
+
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:  # if not isinstance(b, (int, float)):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
