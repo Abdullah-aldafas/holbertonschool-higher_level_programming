275bbaa40cbea817c2b9dbe83b6954c884d186fa
Abdullah <assf23134@icloud.com>
2025-07-26 14:17:15 +0000

    solved Task:0-add two Number

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,7 +10,7 @@ def add_integer(a, b=98):
     Adds two integers or floats and returns the result as an integer.
     """  
     if type(a) is not int and type(a) is not float:  #  if not isinstance(a, (int, float)):
-        raise TypeError("a and b must be integers")
+        raise TypeError("a must be an integers")
     if type(b) is not int and type(b) is not float:  # if not isinstance(b, (int, float)):
-        raise TypeError("a and b must be integers")
+        raise TypeError(" b must be an integers")
     return int(a) + int(b)
