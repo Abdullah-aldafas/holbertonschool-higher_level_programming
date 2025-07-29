7911ae4f2566a3b6cf7be8280975392d656631e4
Abdullah <Assf23134@icloud.com>
2025-07-29 07:49:31 +0000

    Solved Task:3. Area of a square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""This module defines a Square class with size validation."""
+"""
+This module defines a Square class with size validation.
+"""
 
 
 class Square:
     """Represents a square with a private size attribute."""
-
     def __init__(self, size=0):
         """Initialize the square with an optional size."""
         if type(size) is not int:
