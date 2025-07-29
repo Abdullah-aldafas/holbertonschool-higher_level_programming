d4faa4e388692a7aa16ade19e423b856ae233ad4
Abdullah <Assf23134@icloud.com>
2025-07-29 07:48:20 +0000

    Solved Task:3. Area of a square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -9,7 +9,7 @@ class Square:
         """Initialize the square with an optional size."""
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
     
