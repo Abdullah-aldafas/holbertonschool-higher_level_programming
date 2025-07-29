75ba827e3b981d06b090ed913d181979486c9cad
Abdullah <Assf23134@icloud.com>
2025-07-29 08:43:08 +0000

    5. Printing a square

---
+++ python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class with size validation.
+"""
+
+
+class Square:
+    """Represents a square with a private size attribute."""
+
+    def __init__(self, size=0):
+        """Initialize the square with an optional size."""
+        self.size = size
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """Initialize the square with an optional size."""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    def area(self):
+        """Returns the current area of the square."""
+        return self.__size * self.__size
+    def my_print(self):
+        """Prints the square with the character #"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
