4a6e90717ed708039f4b34794525eeed1969631a
Abdullah <Assf23134@icloud.com>
2025-07-29 10:41:38 +0000

    Solved Task:5. Printing a square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -5,15 +5,16 @@ This module defines a Square class with size validation.
 
 
 class Square:
-    """Represents a square with a private size attribute."""
-
+    """a Square class"""
     def __init__(self, size=0):
         """Initialize the square with an optional size."""
         self.size = size
+
     @property
     def size(self):
         """a getter function for size."""
         return self.__size
+
     @size.setter
     def size(self, value):
         """Set the size of the square."""
@@ -22,9 +23,11 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         """Returns the current area of the square."""
         return self.__size * self.__size
+
     def my_print(self):
         """Prints the square with the character # to stdout."""
         if self.__size == 0:
