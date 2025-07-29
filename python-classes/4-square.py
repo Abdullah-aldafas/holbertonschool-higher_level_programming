8ba1671f625a7ade6455a1c512e0bb8276a7922b
Abdullah <Assf23134@icloud.com>
2025-07-29 08:28:34 +0000

    Solved Task:4. Access and update private attribute

---
+++ python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class with size validation.
+"""
+
+
+class Square:
+    """Represents a square with a private size attribute."""
+    def __init__(self, size=0):
+        """Initialize the square with an optional size."""
+        self.size = size
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """Initialize the square with an optional size."""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    def area(self):
+        """Returns the current area of the square."""
+        return self.__size * self.__size
