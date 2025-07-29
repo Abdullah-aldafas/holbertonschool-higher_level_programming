5299e23be1fd265a8b31912d429a4734aaf13e4a
Abdullah <Assf23134@icloud.com>
2025-07-29 08:46:37 +0000

    Update and solved Task:5. Printing a square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
 """
-This module defines a Square class that validates its size and can compute its area.
+This module defines a Square class with size validation.
 """
 
+
 class Square:
-    """
-    Represents a square. The size attribute is private and validated.
-    """
+    """Represents a square with a private size attribute."""
     def __init__(self, size=0):
-        """
-        Initialize the square with an optional size.
-        Size must be an integer and greater than or equal to zero.
-        """
+        """Initialize the square with an optional size."""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
     def area(self):
-        """
-        Returns the current area of the square.
-        """
+        """Returns the current area of the square."""
         return self.__size * self.__size
