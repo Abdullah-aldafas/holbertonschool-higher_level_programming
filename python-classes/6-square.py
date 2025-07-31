78b83b5e54dd4ef13612e083f84498f3a822f452
Abdullah <assf23134@icloud.com>
2025-07-31 09:45:22 +0000

    Solved task:6. Coordinates of a square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -12,12 +12,10 @@ class Square:
         self.size = size
         self.position = position
 
-
     @property
     def size(self):
         """a getter function for size."""
         return self.__size
-
 
     @size.setter
     def size(self, value):
@@ -28,29 +26,28 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-
     @property
     def position(self):
         """a getter function for position."""
         return self.__position
 
-
     @position.setter
     def position(self, value):
         """Set the position of the square."""
-        if (type(value) is not tuple or
-        len(value) != 2 or
-        type(value[0]) is not int or
-        type(value[1]) is not int or
-        value[0] < 0 or value[1] < 0):
+        if (
+            type(value) is not tuple
+            or len(value) != 2
+            or type(value[0]) is not int
+            or type(value[1]) is not int
+            or value[0] < 0
+            or value[1] < 0
+        ):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
-
 
     def area(self):
         """Returns the current area of the square."""
         return self.__size * self.__size
-
 
     def my_print(self):
         """a function that prints a # = to size"""
@@ -60,6 +57,6 @@ class Square:
 
         for i in range(self.__position[1]):
             print()
-            
+
         for j in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
