d798be0eabeb369557717a98b573fc01605a188d
Abdullah <Assf23134@icloud.com>
2025-08-08 14:52:09 +0000

    Solved Task:4. Only sub class of

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,10 +3,10 @@
 
 
 def inherits_from(obj, a_class):
-    """a function that returns True if the object is an instance of a class that inherited"""
-    if type(obj) is a_class: #return isinstance(obj, a_class) and type(obj) is not a_class
+    """a function that returns True if the object of a class that inherited"""
+    if type(obj) is a_class:
         return False
     elif isinstance(obj, a_class):
         return True
     else:
-       return False
+        return False
