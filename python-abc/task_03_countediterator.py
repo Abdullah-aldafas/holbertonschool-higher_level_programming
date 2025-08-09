0d7770ffe292a69520aaef01a92a8d76cbc12ca4
Abdullah <Assf23134@icloud.com>
2025-08-09 11:24:59 +0000

    Solved Task:3. CountedIterator - Keeping Track of Iteration

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+class CountedIterator:
+    def __init__(self, iterator, counter=0):
+        self.iterator = iter(iterator)
+        self.counter = counter
+
+    def get_count(self):
+        return self.counter
+
+    def __next__(self):
+        self.counter += 1
+        return next(self.iterator)
