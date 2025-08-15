c9213d04b6bb825f0c6bd48069e64ebce480ee6a
Abdullah <assf23134@icloud.com>
2025-08-15 12:32:31 +0000

    solved task 7. Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,11 +4,11 @@
 """Add all command-line arguments to a list and save it to add_item.json."""
 
 
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 import sys
 import os
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 
 filename = "add_item.json"
 
