584c00d73227cb7a8b1a0b9227b84f9a35ffe86e
Abdullah <assf23134@icloud.com>
2025-08-15 15:06:54 +0000

    Solved Task 12. Pascal's Triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -13,6 +13,7 @@ def pascal_triangle(n):
             triangle.append([1])
         else:
             prev_row = triangle[-1]
-            middle = [prev_row[j] + prev_row[j + 1] for j in range(len(prev_row) - 1)]
+            middle = [prev_row[j] + prev_row[j + 1] 
+            for j in range(len(prev_row) - 1)]
             triangle.append([1] + middle + [1])
     return triangle
