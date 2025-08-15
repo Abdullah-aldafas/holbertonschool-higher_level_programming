aa306309af91808340c506d27a3363059b262533
Abdullah <assf23134@icloud.com>
2025-08-15 11:40:50 +0000

    Solved Task 3. To JSON string

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -5,9 +5,10 @@ Module that provides a function to convert Python objects to JSON strings.
 """
 import json as j
 
+
 def to_json_string(my_obj):
     """
     Return the JSON string representation of a Python object.
     """
     ra = j.dumps(my_obj)
-    return ra   # return j.dumps(my_obj)   the same solv
+    return ra  # return j.dumps(my_obj)   the same solv
