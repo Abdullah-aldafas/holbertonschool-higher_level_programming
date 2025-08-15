42f804ad096395d909d9df9487f6fd2cc2c37091
Abdullah <assf23134@icloud.com>
2025-08-15 11:24:45 +0000

    Solved Task 1. Write to a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,7 +7,8 @@ Module that provides a helper to write text into a file.
 
 def write_file(filename="", text=""):
     """
-    writes a string to a text file (UTF8) and returns the number of characters written.
+    Write a string to a text file (UTF-8) and return
+    the number of characters written.
     """
 
     with open(filename, "w", encoding="UTF8") as f:
