a387ceb46de108d34a7343671466f96657bc7cb2
Abdullah <assf23134@icloud.com>
2025-09-06 13:44:00 +0000

    Solved Task 7. Cities table

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,6 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 use hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
     id INT UNIQUE AUTO_INCREMENT NOT null PRIMARY KEY,
-    state_id INT NOT null FOREIGN KEY(state_id) REFERENCES states (id),
+    state_id INT NOT null, 
+    FOREIGN KEY(state_id) REFERENCES states (id),
     name VARCHAR(256) NOT null
 );
