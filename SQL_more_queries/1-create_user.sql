117545c494d7e026e319eba58c80dc684f61e1de
Abdullah <assf23134@icloud.com>
2025-09-06 12:20:04 +0000

    Solved Task 1. Root user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 --  that creates the MySQL server user user_0d_1 --
-CREATES USER IF NOT EXISTS  'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANTS ALL ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS  'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
