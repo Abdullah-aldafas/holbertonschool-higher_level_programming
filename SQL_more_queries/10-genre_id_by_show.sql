87c1ee6e6df154c3ecd5dd23096a5f86ecb36822
Abdullah <assf23134@icloud.com>
2025-09-06 14:12:53 +0000

    Solved Task 10. Genre ID by show

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 --  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked --
-SELECT tv_shows.title , tv_show_genres.genre_id FROM tv_shows t , tv_show_genres ts
+SELECT t.title , ts.genre_id FROM tv_shows t , tv_show_genres ts
 WHERE t.id = ts.show_id
 ORDER BY t.title ASC , ts.genre_id ASC;
