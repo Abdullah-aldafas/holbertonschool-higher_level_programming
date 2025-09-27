ef8cce1bb55d7600b400008c97bee023bb3eb75d
Abdullah <Assf23134@icloud.com>
2025-09-27 13:17:05 +0000

    Solved Task 0 and Add README

---
+++ python-server_side_rendering/task_00_intro.py
@@ -0,0 +1,27 @@
+import os 
+
+def generate_invitations(template ,attendees):
+    if isinstance(template, str):
+        print(f"The template must be a string")
+        return
+    if isinstance(attendees, list):
+        print("Attendees must be a list of dictionaries ")
+        return
+    
+    if not template:
+        print("Template is empty, no output files generated.")
+        return
+    if not attendees:
+        print("No data provided, no output files generated.")
+        return
+    for index, attendee in enumerate(attendees, start=1):
+        output_text = template.replace("{name}", attendee.get("name", "N/A"))
+        output_text = output_text.replace("{event_title}", attendee.get("event_title", "N/A"))
+        output_text = output_text.replace("{event_date}", attendee.get("event_date", "N/A"))
+        output_text = output_text.replace("{event_location}", attendee.get("event_location", "N/A"))
+
+        filename = f"output_{index}.txt"
+        with open(filename, "w") as file:
+            file.write(output_text)
+
+        print(f"Created {filename}")
