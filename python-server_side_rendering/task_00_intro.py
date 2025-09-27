7e5af3cedc8bdfd4229764b9301cf99a66049af4
Abdullah <Assf23134@icloud.com>
2025-09-27 13:21:59 +0000

    Solved Task 0 and Add README

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,24 +1,30 @@
-import os 
+import os
 
-def generate_invitations(template ,attendees):
-    if isinstance(template, str):
-        print(f"The template must be a string")
-        return
-    if isinstance(attendees, list):
-        print("Attendees must be a list of dictionaries ")
-        return
+def generate_invitations(template, attendees):
     
+    if not isinstance(template, str):
+        print("The template must be a string")
+        return
+
+    if not isinstance(attendees, list) or not all(isinstance(a, dict) for a in attendees):
+        print("Attendees must be a list of dictionaries")
+        return
+
+   
     if not template:
         print("Template is empty, no output files generated.")
         return
     if not attendees:
         print("No data provided, no output files generated.")
         return
+
+    
     for index, attendee in enumerate(attendees, start=1):
-        output_text = template.replace("{name}", attendee.get("name", "N/A"))
-        output_text = output_text.replace("{event_title}", attendee.get("event_title", "N/A"))
-        output_text = output_text.replace("{event_date}", attendee.get("event_date", "N/A"))
-        output_text = output_text.replace("{event_location}", attendee.get("event_location", "N/A"))
+        output_text = template
+        output_text = output_text.replace("{name}", str(attendee.get("name") or "N/A"))
+        output_text = output_text.replace("{event_title}", str(attendee.get("event_title") or "N/A"))
+        output_text = output_text.replace("{event_date}", str(attendee.get("event_date") or "N/A"))
+        output_text = output_text.replace("{event_location}", str(attendee.get("event_location") or "N/A"))
 
         filename = f"output_{index}.txt"
         with open(filename, "w") as file:
